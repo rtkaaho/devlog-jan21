cd8da40feb80caecdd2784167fed6e21e061027a
adamaho <adam.aho@hey.com>
2021-02-25 15:37:12 -0500

    fixing tailwind config issue

--- kernal/tailwind.config.js
+++ kernal/tailwind.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  presets: [
-    require('@rtkwlf/design-system/dist/tailwind.config.js')
-  ],
   darkMode: 'media',
   purge: ['./pages/**/*.js', './components/**/*.js'],
   theme: {
